7d72e6f19d399ca6db23f0be9bbf02af802ee31c
MichaS-git <s-micha@gmx.net>
2022-11-15 13:47:12 +0100

    added PS 10-32 CANopen Networking and possibility to use/not use Limits;

--- owisApp/src/PS10Driver.cpp
+++ owisApp/src/PS10Driver.cpp
@@ -10,6 +10,7 @@ July 11, 2022
 
 #include <stdio.h>
 #include <string.h>
+#include <sstream>
 #include <stdlib.h>
 #include <math.h>
 
@@ -25,12 +26,14 @@ July 11, 2022
 
 /** Creates a new PS10Controller object.
   * \param[in] portName          The name of the asyn port that will be created for this driver
-  * \param[in] PS10PortName     The name of the drvAsynSerialPort that was created previously to connect to the PS10 controller
+  * \param[in] PS10PortName      The name of the drvAsynSerialPort that was created previously to connect to the PS10 controller
   * \param[in] numAxes           The number of axes that this controller supports
+  * \param[in] slaveIDs          List of SlaveIDs when using the PS 10-32 in CAN-Daysy-Chain, separated by coma
+  * \param[in] actLimits         String for Limit-activation (y) and -deactivation (n), e.g. "ny" for two axes
   * \param[in] movingPollPeriod  The time between polls when any axis is moving
   * \param[in] idlePollPeriod    The time between polls when no axis is moving
   */
-PS10Controller::PS10Controller(const char *portName, const char *PS10PortName, int numAxes,
+PS10Controller::PS10Controller(const char *portName, const char *PS10PortName, int numAxes, const char *slaveIDs, const char *actLimits,
                                double movingPollPeriod, double idlePollPeriod)
     :  asynMotorController(portName, numAxes, NUM_PS10_PARAMS,
                            0, // No additional interfaces beyond those in base class
@@ -40,9 +43,24 @@ PS10Controller::PS10Controller(const char *portName, const char *PS10PortName, i
                            0, 0)  // Default priority and stack size
 {
     int axis;
+    int* slaveIDsArr = new int[numAxes];
     asynStatus status;
     PS10Axis *pAxis;
     static const char *functionName = "PS10Controller::PS10Controller";
+
+    // convert the slaveIDs to an integer array
+    std::string str = slaveIDs;
+    std::stringstream text_stream(slaveIDs);
+    std::string item;
+
+    int i = 0;
+    while (std::getline(text_stream, item, ','))
+    {
+        slaveIDsArr[i] = std::atoi(item.c_str());
+        i++;
+    }
+
+    std::string limits = actLimits;
 
     /* Connect to PS10 controller */
     status = pasynOctetSyncIO->connect(PS10PortName, 0, &pasynUserController_, NULL);
@@ -54,7 +72,7 @@ PS10Controller::PS10Controller(const char *portName, const char *PS10PortName, i
     }
     for (axis=0; axis<numAxes; axis++)
     {
-        pAxis = new PS10Axis(this, axis);
+        pAxis = new PS10Axis(this, axis, slaveIDsArr[axis], limits[axis]);
     }
 
     startPoller(movingPollPeriod, idlePollPeriod, 2);
@@ -66,14 +84,16 @@ PS10Controller::PS10Controller(const char *portName, const char *PS10PortName, i
   * \param[in] portName          The name of the asyn port that will be created for this driver
   * \param[in] PS10PortName      The name of the drvSerialPort that was created previously to connect to the PS10 controller
   * \param[in] numAxes           The number of axes that this controller supports
+  * \param[in] slaveIDs          List of SlaveIDs when using the PS 10-32 in CAN-Daysy-Chain, separated by coma
+  * \param[in] actLimits         String for Limit-activation (y) and -deactivation (n), e.g. "ny" for two axes
   * \param[in] movingPollPeriod  The time in ms between polls when any axis is moving
   * \param[in] idlePollPeriod    The time in ms between polls when no axis is moving
   */
-extern "C" int PS10CreateController(const char *portName, const char *PS10PortName, int numAxes,
+extern "C" int PS10CreateController(const char *portName, const char *PS10PortName, int numAxes, const char *slaveIDs, const char *actLimits,
                                     int movingPollPeriod, int idlePollPeriod)
 {
     PS10Controller *pPS10Controller
-        = new PS10Controller(portName, PS10PortName, numAxes, movingPollPeriod/1000., idlePollPeriod/1000.);
+        = new PS10Controller(portName, PS10PortName, numAxes, slaveIDs, actLimits, movingPollPeriod/1000., idlePollPeriod/1000.);
     pPS10Controller = NULL;
     return(asynSuccess);
 }
@@ -89,28 +109,11 @@ void PS10Controller::report(FILE *fp, int level)
 {
     fprintf(fp, "PS 10 motor driver\n");
     fprintf(fp, "  port name=%s\n", this->portName);
-    fprintf(fp, "  num axes=%d\n", numAxes_);
     fprintf(fp, "  moving poll period=%f\n", movingPollPeriod_);
     fprintf(fp, "  idle poll period=%f\n", idlePollPeriod_);
 
     // Call the base class method
     asynMotorController::report(fp, level);
-}
-
-/** Returns a pointer to an PS10Axis object.
-  * Returns NULL if the axis number encoded in pasynUser is invalid.
-  * \param[in] pasynUser asynUser structure that encodes the axis index number. */
-PS10Axis* PS10Controller::getAxis(asynUser *pasynUser)
-{
-    return static_cast<PS10Axis*>(asynMotorController::getAxis(pasynUser));
-}
-
-/** Returns a pointer to an PS10Axis object.
-  * Returns NULL if the axis number encoded in pasynUser is invalid.
-  * \param[in] axisNo Axis index number. */
-PS10Axis* PS10Controller::getAxis(int axisNo)
-{
-    return static_cast<PS10Axis*>(asynMotorController::getAxis(axisNo));
 }
 
 /** Writes a string to the controller and reads the response.
@@ -176,10 +179,10 @@ asynStatus PS10Controller::writeController(const char *output, double timeout)
 /** Creates a new PS10Axis object.
   * \param[in] pC Pointer to the PS10Controller to which this axis belongs.
   * \param[in] axisNo Index number of this axis, range 0 to pC->numAxes_-1.
-  *
+  * \param[in] slaveID when operating in daisy chain.
   * Initializes register numbers, etc.
   */
-PS10Axis::PS10Axis(PS10Controller *pC, int axisNo)
+PS10Axis::PS10Axis(PS10Controller *pC, int axisNo, int slaveID, const char actLimit)
     : asynMotorAxis(pC, axisNo),
       pC_(pC),
       velocityMode(false)
@@ -189,40 +192,55 @@ PS10Axis::PS10Axis(PS10Controller *pC, int axisNo)
     static const char *functionName = "PS10Axis::PS10Axis";
 
     // controller axes are numbered from 1
-    axisIndex_ = axisNo + 1;
+    //axisIndex_ = axisNo + 1;
+
+    // Slave-ID for CAN-Daysy-Chain, 00 to 99
+    slaveID_ = slaveID;
 
     // Flush I/O in case there is lingering garbage
     pC_->writeReadController();
 
     // Read the status of this motor and initialize the axis if not initialized
-    sprintf(pC_->outString_, "?ASTAT");
+    sprintf(pC_->outString_, "%02d?ASTAT", slaveID_);
     status = pC_->writeReadController();
     if (status != asynSuccess)
         errorFlag = 1;
+
     if (strncmp(pC_->inString_, "I", 1) == 0)
-        sprintf(pC_->outString_, "INIT%d", axisIndex_);
-    status = pC_->writeController();
-    if (status != asynSuccess)
-        errorFlag = 1;
+        sprintf(pC_->outString_, "%02dINIT1", slaveID_);
+        status = pC_->writeController();
+        if (status != asynSuccess)
+            errorFlag = 1;
 
     // Read the version string and the motor type
-    sprintf(pC_->outString_, "?MOTYPE%d", axisIndex_);
+    sprintf(pC_->outString_, "%02d?MOTYPE1", slaveID_);
     status = pC_->writeReadController();
     if (status != asynSuccess)
         errorFlag = 1;
     // Store motor type
     motorType_ = atoi(&pC_->inString_[1]);
 
+    // Set limit switch mask, use MAXSTOP and MINSTOP if actLimit is 'y'
+    if (actLimit == 'y')
+    {
+        sprintf(pC_->outString_, "%02dSMK1=9", slaveID_);
+    }
+    else
+    {
+        sprintf(pC_->outString_, "%02dSMK1=0", slaveID_);
+    }
+    status = pC_->writeController();
+    if (status != asynSuccess)
+        errorFlag = 1;
+
     // What should happen if the controller doesn't respond?
     // For now put the controller in an error state
-
     if (errorFlag == 1)
     {
         setIntegerParam(pC_->motorStatusProblem_, 1);
     }
 
     callParamCallbacks();
-
 }
 
 /** Reports on status of the axis
@@ -235,8 +253,8 @@ void PS10Axis::report(FILE *fp, int level)
 {
     if (level > 0)
     {
-        fprintf(fp, "  axis %d\n", axisNo_);
-        fprintf(fp, "  axis index %d\n", axisIndex_);
+        //fprintf(fp, "  axis %d\n", axisNo_);
+        fprintf(fp, "  slave ID=%d\n", slaveID_);
         fprintf(fp, "  motor type %d\n", motorType_);
     }
 
@@ -250,11 +268,11 @@ asynStatus PS10Axis::sendAccelAndVelocity(double acceleration, double velocity)
     // static const char *functionName = "PS10::sendAccelAndVelocity";
 
     // Send the velocity
-    sprintf(pC_->outString_, "PVEL%d=%d", axisIndex_, NINT(velocity));
+    sprintf(pC_->outString_, "%02dPVEL1=%d", slaveID_, NINT(velocity));
     status = pC_->writeController();
 
     // Send the acceleration
-    sprintf(pC_->outString_, "ACC%d=%d", axisIndex_, NINT(acceleration));
+    sprintf(pC_->outString_, "%02dACC1=%d", slaveID_, NINT(acceleration));
     status = pC_->writeController();
 
     return status;
@@ -270,18 +288,18 @@ asynStatus PS10Axis::move(double position, int relative, double minVelocity, dou
     velocityMode = false;
     if (relative)
     {
-        sprintf(pC_->outString_, "RELAT%d", axisIndex_);
+        sprintf(pC_->outString_, "%02dRELAT1", slaveID_);
     }
     else
     {
-        sprintf(pC_->outString_, "ABSOL%d", axisIndex_);
+        sprintf(pC_->outString_, "%02dABSOL1", slaveID_);
     }
     status = pC_->writeController();
 
-    sprintf(pC_->outString_, "PSET%d=%d", axisIndex_, NINT(position));
+    sprintf(pC_->outString_, "%02dPSET1=%d", slaveID_, NINT(position));
     status = pC_->writeController();
 
-    sprintf(pC_->outString_, "PGO%d", axisIndex_);
+    sprintf(pC_->outString_, "%02dPGO1", slaveID_);
     status = pC_->writeController();
 
     return status;
@@ -297,16 +315,16 @@ asynStatus PS10Axis::home(double minVelocity, double maxVelocity, double acceler
     velocityMode = false;
     if (forwards)
     {
-        sprintf(pC_->outString_, "RMK%d=1000", axisIndex_);
+        sprintf(pC_->outString_, "%02dRMK1=8", slaveID_);
     }
     else
     {
-        sprintf(pC_->outString_, "RMK%d=0001", axisIndex_);
+        sprintf(pC_->outString_, "%02dRMK1=1", slaveID_);
     }
     status = pC_->writeController();
 
     // we use the mode 4 only so far
-    sprintf(pC_->outString_, "REF%d=4", axisIndex_);
+    sprintf(pC_->outString_, "%02dREF1=4", slaveID_);
     status = pC_->writeController();
 
     return status;
@@ -325,10 +343,10 @@ asynStatus PS10Axis::moveVelocity(double minVelocity, double maxVelocity, double
 
     velocityMode = true;
 
-    sprintf(pC_->outString_, "VVEL%d=%f", axisIndex_, minVelocity);
+    sprintf(pC_->outString_, "%02dVVEL1=%f", slaveID_, minVelocity);
     status = pC_->writeController();
 
-    sprintf(pC_->outString_, "VGO%d", axisIndex_);
+    sprintf(pC_->outString_, "%02dVGO1", slaveID_);
     status = pC_->writeController();
 
     return status;
@@ -339,9 +357,9 @@ asynStatus PS10Axis::stop(double acceleration )
     asynStatus status;
     //static const char *functionName = "PS10Axis::stop";
     if(velocityMode)
-        sprintf(pC_->outString_, "VSTP%d", axisIndex_);
+        sprintf(pC_->outString_, "%02dVSTP1", slaveID_);
     else
-        sprintf(pC_->outString_, "STOP%d", axisIndex_);
+        sprintf(pC_->outString_, "%02dSTOP1", slaveID_);
 
     status = pC_->writeController();
     velocityMode = false;
@@ -356,46 +374,79 @@ asynStatus PS10Axis::stop(double acceleration )
 asynStatus PS10Axis::poll(bool *moving)
 {
     int done = 0;
-    //int limit;
+    int limit = 0;
     int position;
     asynStatus comStatus;
 
+    /*
+    // Read out the message exit buffer
+    sprintf(pC_->outString_, "%02d?MSG", slaveID_);
+    comStatus = pC_->writeReadController();
+    if (comStatus) goto skip;
+
+    // Read out the error messages
+    sprintf(pC_->outString_, "%02d?ERR", slaveID_);
+    comStatus = pC_->writeReadController();
+    if (comStatus) goto skip;
+
+    // Read out the limit switch mask
+    sprintf(pC_->outString_, "%02d?SMK1", slaveID_);
+    comStatus = pC_->writeReadController();
+    if (comStatus) goto skip;
+    */
+
     // Read the current motor position
-    sprintf(pC_->outString_, "?CNT%d", axisIndex_);
+    sprintf(pC_->outString_, "%02d?CNT1", slaveID_);
     comStatus = pC_->writeReadController();
     if (comStatus) goto skip;
     // The response string is of the form "1000"
-    //position = atof((char *)&pC_->inString_);
     sscanf(pC_->inString_, "%d", &position);
     setDoubleParam(pC_->motorPosition_, position);
 
-    // Read the axis state inquiry
-    // response in letters: IIORRTTJV (one letter if there is only one axis)
-    sprintf(pC_->outString_, "?ASTAT");
+    // Read the limit status
+    sprintf(pC_->outString_, "%02d?ESTAT1", slaveID_);
     comStatus = pC_->writeReadController();
     if (comStatus) goto skip;
+    // The response string is of the form "10101"
+    sscanf(pC_->inString_, "%d", &limit);
+    if (limit == 8)
+    {
+        setIntegerParam(pC_->motorStatusHighLimit_, 1);
+    }
+    if (limit == 1)
+    {
+        setIntegerParam(pC_->motorStatusLowLimit_, 1);
+    }
+    if (limit == 0)
+    {
+        setIntegerParam(pC_->motorStatusHighLimit_, 0);
+        setIntegerParam(pC_->motorStatusLowLimit_, 0);
+    }
+
+    // Read the axis state inquiry, response in letters: IIORRTTJV
+    sprintf(pC_->outString_, "%02d?ASTAT", slaveID_);
+    comStatus = pC_->writeReadController();
+    if (comStatus) goto skip;
+    // R means axis initialized and ready
     if (strncmp(pC_->inString_, "R", 1) == 0)
     {
         done = 1;
     }
+    // L means axis has been disabled after approaching a hardware limit switch
+    if (strncmp(pC_->inString_, "L", 1) == 0)
+    {
+        // initialize the axis again and release the limit switch
+        // afterwards the user should reference the axis manually
+        sprintf(pC_->outString_, "%02dINIT1", slaveID_);
+        comStatus = pC_->writeController();
+        if (comStatus) goto skip;
+        sprintf(pC_->outString_, "%02dEFREE1", slaveID_);
+        comStatus = pC_->writeController();
+        if (comStatus) goto skip;
+    }
     setIntegerParam(pC_->motorStatusDone_, done);
     setIntegerParam(pC_->motorStatusMoving_, !done);
     *moving = done ? false:true;
-
-    // Only a rotation motor for testing, no limit switches for testing
-    /*// Read the limit status
-    sprintf(pC_->outString_, "?ESTAT%d", axisIndex_);
-    comStatus = pC_->writeReadController();
-    if (comStatus) goto skip;
-    // The response string is of the form "10101"
-    limit = (pC_->inString_[3] == '1') ? 1:0;
-    std::cout << "high limit "<< limit << std::endl;
-    setIntegerParam(pC_->motorStatusHighLimit_, limit);
-    limit = (pC_->inString_[0] == '1') ? 1:0;
-    std::cout << "low limit "<< limit << std::endl;
-    setIntegerParam(pC_->motorStatusLowLimit_, limit);
-    //limit = (pC_->inString_[3] == '1') ? 1:0;
-    //setIntegerParam(pC_->motorStatusAtHome_, limit);*/
 
 skip:
     setIntegerParam(pC_->motorStatusProblem_, comStatus ? 1:0);
@@ -407,18 +458,22 @@ skip:
 static const iocshArg PS10CreateControllerArg0 = {"Port name", iocshArgString};
 static const iocshArg PS10CreateControllerArg1 = {"PS 10 port name", iocshArgString};
 static const iocshArg PS10CreateControllerArg2 = {"Number of axes", iocshArgInt};
-static const iocshArg PS10CreateControllerArg3 = {"Moving poll period (ms)", iocshArgInt};
-static const iocshArg PS10CreateControllerArg4 = {"Idle poll period (ms)", iocshArgInt};
+static const iocshArg PS10CreateControllerArg3 = {"List with slaveIDs", iocshArgString};
+static const iocshArg PS10CreateControllerArg4 = {"List for Limit-activation", iocshArgString};
+static const iocshArg PS10CreateControllerArg5 = {"Moving poll period (ms)", iocshArgInt};
+static const iocshArg PS10CreateControllerArg6 = {"Idle poll period (ms)", iocshArgInt};
 static const iocshArg * const PS10CreateControllerArgs[] = {&PS10CreateControllerArg0,
                                                             &PS10CreateControllerArg1,
                                                             &PS10CreateControllerArg2,
                                                             &PS10CreateControllerArg3,
-                                                            &PS10CreateControllerArg4
+                                                            &PS10CreateControllerArg4,
+                                                            &PS10CreateControllerArg5,
+                                                            &PS10CreateControllerArg6
                                                            };
-static const iocshFuncDef PS10CreateControllerDef = {"PS10CreateController", 5, PS10CreateControllerArgs};
+static const iocshFuncDef PS10CreateControllerDef = {"PS10CreateController", 7, PS10CreateControllerArgs};
 static void PS10CreateContollerCallFunc(const iocshArgBuf *args)
 {
-    PS10CreateController(args[0].sval, args[1].sval, args[2].ival, args[3].ival, args[4].ival);
+    PS10CreateController(args[0].sval, args[1].sval, args[2].ival, args[3].sval, args[4].sval, args[5].ival, args[6].ival);
 }
 
 static void PS10Register(void)
--- owisApp/src/PS10Driver.h
+++ owisApp/src/PS10Driver.h
@@ -10,9 +10,6 @@ July 11, 2022
 #include "asynMotorController.h"
 #include "asynMotorAxis.h"
 
-// No possibility to test the "Networking PS 10-32 CANopen" for up to 32 axes in daisy-chain
-#define MAX_PS10_AXES 1
-
 // No controller-specific parameters yet
 #define NUM_PS10_PARAMS 0
 
@@ -20,7 +17,7 @@ class PS10Axis : public asynMotorAxis
 {
 public:
     /* These are the methods we override from the base class */
-    PS10Axis(class PS10Controller *pC, int axis);
+    PS10Axis(class PS10Controller *pC, int axis, int slaveID, const char actLimit);
     void report(FILE *fp, int level);
     asynStatus move(double position, int relative, double min_velocity, double max_velocity, double acceleration);
     asynStatus moveVelocity(double min_velocity, double max_velocity, double acceleration);
@@ -32,6 +29,7 @@ private:
     PS10Controller *pC_;          /**< Pointer to the asynMotorController to which this axis belongs.
                                    *   Abbreviated because it is used very frequently */
     int axisIndex_;    /* Numbered from 1 */
+    int slaveID_;      /* Slave-ID for CAN-Daysy-Chain, 00 to 99*/
     int motorType_;    /* Motor Type: 0 = DC brush; 1 = stepper motor open-loop */
     bool velocityMode;
     asynStatus sendAccelAndVelocity(double accel, double velocity);
@@ -42,13 +40,10 @@ private:
 class PS10Controller : public asynMotorController
 {
 public:
-    PS10Controller(const char *portName, const char *PS10PortName, int numAxes, double movingPollPeriod, double idlePollPeriod);
-
+    PS10Controller(const char *portName, const char *PS10PortName, int numAxes, const char *slaveIDs, const char *actLimits, double movingPollPeriod, double idlePollPeriod);
 
     /* These are the methods that we override from asynMotorDriver */
     void report(FILE *fp, int level);
-    PS10Axis* getAxis(asynUser *pasynUser);
-    PS10Axis* getAxis(int axisNo);
     asynStatus writeReadController();
     asynStatus writeReadController(const char *output, char *input, size_t maxChars, size_t *nread, double timeout);
     asynStatus writeController();
